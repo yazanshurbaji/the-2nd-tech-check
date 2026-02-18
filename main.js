2c86a68a9a2a57ef198a7669b2513a2704160d77
yazanshurbaji <w0496586@nscc.ca>
2026-02-18 09:44:05 -0400

    ready for marking

--- main.js
+++ main.js
@@ -2,57 +2,72 @@
 
   var DATA_URL = "https://prog2700.onrender.com/dna/";
   var DNA_SEQUENCE = "GTGCCAATGTTACTGCTAAATCTCTATATACAGTGGCTTAAGGATGGGGGGCCCAGCAGCGGCCGACCCCCCCCCTCAGTGTGGAATCAACCGGAATTGAGG";
-  // Step 1: Convert DNA sequence into codons (3 letters each)
+ 
+     // Extract Codons as JavaScript Array from the DNA Sequence String.
     var extractCodonsFromDNA = function(dnaSequence)
     {
         var codons = [];
 
+        // TODO: ADD CODE TO COMPLETE THE FUNCTION HERE...
+        // you'll get an error notification in the console until the function is completed correctly
+
         // remove any hidden spaces or new lines
         dnaSequence = dnaSequence.replace(/\s/g, "");
 
-        for (var i = 0; i + 3 <= dnaSequence.length; i += 3)
-        {
+        // loop through the DNA string, 3 characters at a time
+        for (var i = 0; i + 3 <= dnaSequence.length; i += 3) {
             codons.push(dnaSequence.substring(i, i + 3));
         }
 
         return codons;
     }
 
-    // Step 2: Convert codons into amino abbreviations using JSON data
+    // Compare the Codons array with the map of Amino Acids found in the json data.
+    // Add any matches to the aminos array.
     var translateCodonsToAminos = function(codons, jsonData)
     {
         var aminos = [];
 
+        // TODO: ADD CODE TO COMPLETE THE FUNCTION HERE...
+        // you'll get an error notification in the console until the function is completed correctly
+
+        // create lookup table: codon -> amino abbreviation
         var lookup = {};
-        for (var i = 0; i < jsonData.length; i++)
-        {
+
+        for (var i = 0; i < jsonData.length; i++) {
             var item = jsonData[i];
-            for (var j = 0; j < item.codons.length; j++)
-            {
+
+            for (var j = 0; j < item.codons.length; j++) {
                 lookup[item.codons[j]] = item.abbr;
             }
         }
 
-        for (var k = 0; k < codons.length; k++)
-        {
+        // translate codons using lookup table
+        for (var k = 0; k < codons.length; k++) {
             aminos.push(lookup[codons[k]]);
         }
 
         return aminos;
     }
 
-    var runProgram = function ()
-    {
-        var codons = extractCodonsFromDNA(DNA_SEQUENCE); // DO NOT MODIFY
-        var aminos; // DO NOT MODIFY
+    var runProgram = function () {
+
+        var codons = extractCodonsFromDNA(DNA_SEQUENCE); //DO NOT MODIFY
+        var aminos; //DO NOT MODIFY
+
+        // TODO: ENTER CODE TO LOAD DATA FROM API HERE.
 
         fetch(DATA_URL)
             .then(function(response){
                 return response.json();
             })
             .then(function(json){
-                aminos = translateCodonsToAminos(codons, json);
-                tests.runTests(codons, aminos); // DO NOT MODIFY
+                //ONCE YOU HAVE YOUR API CALL WORKING, UNCOMMENT THE LINE ABOVE THE runTests LINE AND APPLY
+                //BOTH LINES (including the test line) WITHIN THE CODE ABOVE WHERE YOU RECEIVE YOUR JSON DATA FROM YOUR API CALL...
+                //DO NOT MODIFY THE LINES EXCEPT FOR UNCOMMENTING THEM AND MOVING THEM TO THE CORRECT LOCATION ABOVE IN CODE
+
+                aminos = translateCodonsToAminos(codons, json); //DO NOT MODIFY...but you can uncomment and move when ready
+                tests.runTests(codons, aminos); //DO NOT MODIFY...but you can move when ready
             })
             .catch(function(error){
                 console.log("API error:", error);
